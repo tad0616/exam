6cfe8aa3532e14a5cd7e318c63fcf0b1120ec0aa
Tad <tad.learn@gmail.com>
2017-12-14 16:43:59 +0800

    讀出所有測驗以新增題目

---
+++ resources/views/exam/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+    <h1>測驗一覽</h1>
+    <ul class="list-group">
+        @forelse ($exams as $exam)
+            <li class="list-group-item">
+                {{$exam->created_at->format("Y年m月d日")}} -
+                <a href="exam/{{$exam->id}}">
+                {{$exam->title}}
+                </a>
+            </li>
+        @empty 
+            <div class="alert alert-danger">尚無任何測驗</div>
+        @endforelse
+    </ul>
+@endsection
+
