0fc1c1878ebb3a6b887c635cb6f6343184417041
Tad <tad.learn@gmail.com>
2017-12-14 16:03:38 +0800

    建立發布測驗的表單

--- resources/views/exam/create.blade.php
+++ resources/views/exam/create.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <h1>新增測驗</h1>
+    <h1>新增測驗</h1>    
+    @form(['url' => '/exam', 'class' => 'form-horizontal', 'labelWidth' => '2', 'fieldWidth' => '10', 'framework' => 'bootstrap4'])
+        @text('title', '', ['placeholder' => '請輸入測驗標題', 'label' => '測驗標題'])
+        @checkbox('enable', '1', true, ['placeholder' => '是否啟用', 'label' => '啟用此測驗'])
+        @submit('儲存', ['class' => 'btn btn-success'])
+    @endform
 @endsection
