186d8c2ea2be8c55aa01eabfafd97c21585bbc7a
Tad <tad.learn@gmail.com>
2017-12-14 14:32:28 +0800

    製作系統主樣板

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container">
+		<a class="navbar-brand" href="{{ url('/') }}">
+			{{ config('app.name', 'Laravel') }}
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+		 aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+			<ul class="navbar-nav">
+				@if (Auth::guest())
+				<li class="nav-item">
+					<a href="{{ route('login') }}" class="nav-link">登入</a>
+				</li>
+				<li class="nav-item">
+					<a href="{{ route('register') }}" class="nav-link">註冊</a>
+				</li>
+				@else
+				<li class="nav-item dropdown">
+					<a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						{{ Auth::user()->name }}
+					</a>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+						<a href="{{ route('logout') }}" class="dropdown-item" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+							登出
+						</a>
+
+						<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+							{{ csrf_field() }}
+						</form>
+					</div>
+				</li>
+				@endif
+			</ul>
+		</div>
+
+	</div>
+</nav>
